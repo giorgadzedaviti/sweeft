075ba0de3bf422ca5ec4e122648c938acb31e50f
dati giorgadze <83769327+niko940@users.noreply.github.com>
2023-03-18 18:59:14 +0400

    minor changes

--- src/APIComponent.js
+++ src/APIComponent.js
@@ -9,7 +9,6 @@ function APIComponent() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedUserId, setSelectedUserId] = useState(null);
   const [selectedUserData, setSelectedUserData] = useState(null);
-  const [friendPage, setFriendPage] = useState(1);
 
   useEffect(() => {
     fetchData();
@@ -21,6 +20,7 @@ function APIComponent() {
         window.innerHeight + window.pageYOffset >=
         document.body.offsetHeight
       ) {
+        setCurrentPage((prevPage) => prevPage + 1);
       }
     };
 
@@ -60,7 +60,7 @@ function APIComponent() {
           `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${userId}`
         ),
         fetch(
-          `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${userId}/friends/${currentPage}/100`
+          `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${userId}/friends/${currentPage}/12`
         ),
       ])
         .then(([userResponse, friendsResponse]) => {
@@ -72,18 +72,7 @@ function APIComponent() {
         .then(([userData, friendsData]) => {
           console.log(friendsData);
           setLoading(false);
-          let page = 1;
-          const friendListHandlerer = () => {
-            const newFriends = data.list.map(
-              (friend) => `
-                        <div class="friend-container">
-                          <img src="${friend.imageUrl}" alt="${friend.name} class='card-image'">
-                          <h4 class='title-header'>${friend.prefix} ${friend.name} ${friend.lastName}</h4>
-                          <p class='title-text'>${friend.title}</p>
-                        </div>
-                      `
-            );
-          };
+          const friendListHandlerer = () => {};
           document.write(`
             <html>
               <head>
@@ -92,125 +81,109 @@ function APIComponent() {
               <body>
 
               <style>
-                    body {
-                      font-family: Arial, sans-serif;
-                      font-size: 16px;
-                      line-height: 1.4;
-                      margin: 0;
-                      padding: 0;
-                      
-                    }
-
-                    .container {
-                      display: flex;
-                      flex-direction: column;
-                      padding-left: 100px;
-                      padding-right: 100px;
-                    }
-
-                    .info {
-                      margin-left: 20px;
-                    }
-
-                    h1 {
-                      font-size: 24px;
-                      margin-bottom: 10px;
-                    }
-
-                    img {
-                      height: auto;
-                      margin-top: 30px;
-                      margin-left:50px;
-                      height: 200px;
-                    }
-
-                    p {
-                      margin-bottom: 10px;
-                    }
-                    .address{
-                      margin-left:300px;
-                      max-width:200px;
-                      margin-top:-300px;
-                      font-size:16px;
-
-                    }
-                    .user{
-                      margin-top:10px;
-                      margin-left: -60px;
-                    }
-                    .nm{
-                      margin-bottom:-20px;
-                      margin-top:30px
-                    }
-
-                    .info{
-                    margin-top:10px
-
-                    }
-                    .info1{
-                    margin-bottom:-25px;
-                    // margin-left:10px
-
-
-                    }
-                    .address1{
-                    margin-bottom:-25px;
-                    margin-top:30px;
-                    margin-left:10px
-
-                    }
-                    .company{
-                    margin-bottom:-15px;
-                    margin-top:30px;
-                    } 
-
-                    .friends {
-                    font-size: 16px;
-                    padding: 10px;
-                  
-                    display: grid;
-                    grid-template-columns: repeat(4, minmax(0, 1fr));
-                    grid-gap: 100px;
-
-                    }
-                    .friend-container{
-                      display: flex;
-                      justify-content: space-around;
-                      align-items: center;
-                      flex-direction: column;
-                    }
-                    .title-header{
-                      margin-top: 0px;
-                    }
-                    .title-text{
-                      margin-top: -23px;
-                    }
-
-                    .name2 {
-                    margin-bottom: 252px; // Remove this line if you want the friends to display side by side
-                    }
-
-                    .img2 {
-                    height: 200px;
-                    margin-left: 10px; // Change the margin to 10px to give some space between friends
-                    object-fit: cover;
-                    }
-
-                    .title1 {
-                    margin-top: 50px;
-                    margin-left: 0px; // Add this line to align the title with the friend's name and image
-                    }
-                    .card-image{
-                      object-fit: cover;
-                    }
-                    .flex-row-cont{
-                      display: flex;
-                      justify-contet: space-around
-                      flex-direction: row;
-                    }
-                    .img-cont{
-                      width: 50%;
-                    }
+              body {
+                font-family: Arial, sans-serif;
+                font-size: 16px;
+                line-height: 1.4;
+                margin: 0;
+                padding: 0;
+                
+              }
+              .container {
+                display: flex;
+                flex-direction: column;
+                padding-left: 100px;
+                padding-right: 100px;
+              }
+              .info {
+                margin-left: 20px;
+              }
+              h1 {
+                font-size: 24px;
+                margin-bottom: 10px;
+              }
+              img {
+                height: auto;
+                margin-top: 30px;
+                margin-left:50px;
+                height: 200px;
+              }
+              p {
+                margin-bottom: 10px;
+              }
+              .address{
+                margin-left:300px;
+                max-width:200px;
+                margin-top:-300px;
+                font-size:16px;
+              }
+              .user{
+                margin-top:10px;
+                margin-left: -60px;
+              }
+              .nm{
+                margin-bottom:-20px;
+                margin-top:30px
+              }
+              .info{
+              margin-top:10px
+              }
+              .info1{
+              margin-bottom:-25px;
+              // margin-left:10px
+              }
+              .address1{
+              margin-bottom:-25px;
+              margin-top:30px;
+              margin-left:10px
+              }
+              .company{
+              margin-bottom:-15px;
+              margin-top:30px;
+              } 
+              .friends {
+              font-size: 16px;
+              padding: 10px;
+            
+              display: grid;
+              grid-template-columns: repeat(4, minmax(0, 1fr));
+              grid-gap: 100px;
+              }
+              .friend-container{
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+                flex-direction: column;
+              }
+              .title-header{
+                margin-top: 0px;
+              }
+              .title-text{
+                margin-top: -23px;
+              }
+              .name2 {
+              margin-bottom: 252px; // Remove this line if you want the friends to display side by side
+              }
+              .img2 {
+              height: 200px;
+              margin-left: 10px; // Change the margin to 10px to give some space between friends
+              object-fit: cover;
+              }
+              .title1 {
+              margin-top: 50px;
+              margin-left: 0px; // Add this line to align the title with the friend's name and image
+              }
+              .card-image{
+                object-fit: cover;
+              }
+              .flex-row-cont{
+                display: flex;
+                justify-contet: space-around
+                flex-direction: row;
+              }
+              .img-cont{
+                width: 50%;
+              }
               </style>
               <div class="container">
                     <div class='flex-row-cont'>
@@ -233,7 +206,6 @@ function APIComponent() {
                           <p>Job Type: ${userData.jobType}</p>
                           <p>Job Description: ${userData.jobDescriptor}</p>
                       </div>
-
                       <div class="address">
                         <p class="address1">address</p>
                         <h3 class="company">${userData.company.name}</h1>
@@ -244,7 +216,6 @@ function APIComponent() {
                         <p>ZIP: ${userData.address.zipCode}</p>
                       </div>
                   </div>
-
               </div>
               <p>Friends:</p>
             
@@ -268,20 +239,20 @@ function APIComponent() {
             
             <script>
               const loadingElement = document.getElementById('loading');
-              let page = 1;
+              let page = 2;
             
               function loadMore() {
                 loadingElement.style.display = 'block';
             
                 // simulate loading more data from the server
                 setTimeout(() => {
-                  fetch('http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${userId}/friends/${page}/12')
+                  fetch('https://example.com/friends?page=')
                     .then(response => response.json())
                     .then(data => {
                       const friendContainer = document.querySelector('.friends');
-                      ${friendListHandlerer}
+                      friendListHandlerer()
                       friendContainer.insertAdjacentHTML('beforeend', newFriends.join(''));
-                      ${page++};
+                      page++;
                       loadingElement.style.display = 'none';
                     })
                     .catch(error => {
